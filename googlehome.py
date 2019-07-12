ee7947df69cc749cb864e3263262fafd0d5343e4
Ryoto Saito <ryoto.no.address@gmail.com>
2019-07-13 02:11:23 +0900

    avoid beep

--- googlehome.py
+++ googlehome.py
@@ -3,6 +3,9 @@ from gtts import gTTS
 from io import BytesIO
 from pychromecast import get_chromecasts
 import socket
+import time
+
+chromecasts = get_chromecasts()
 
 def is_ip_addr(str):
     try:
@@ -16,29 +19,31 @@ def is_ip_addr(str):
             return False
 
 class GoogleHome():
-    devices = []
-
     def __init__(self, ip_or_name):
-        if len(self.devices) == 0:
-            self.get_devices()
         if is_ip_addr(ip_or_name):
             ip = ip_or_name
-            filtered_devices = list(filter(lambda x: x.host == ip, devices))
+            devices = list(filter(lambda x: x.host == ip, chromecasts))
         else:
             name = ip_or_name
-            filtered_devices = list(filter(lambda x: x.device.friendly_name == name, devices))
+            devices = list(filter(lambda x: x.device.friendly_name == name, chromecasts))
         if len(devices) == 0:
             raise RuntimeError("Device named " + ip_or_name + " not found")
         self.dev = devices[0]
-
-    @classmethod
-    def get_devices(cls):
-        cls.devices = get_chromecasts()
 
     def read_message(self, msg, lang):
         fp = BytesIO()
         gTTS(msg, lang=lang).write_to_fp(fp)
         fp.seek(0)
+        self.dev.wait()
+        volume = self.dev.status.volume_level
+        self.dev.set_volume(0.0)
         mc = self.dev.media_controller
-        mc.play_media('data:audio/mp3;base64,' + b64encode(fp.read()).decode('UTF-8'), 'audio/mp3')
-
+        mc.play_media('data:audio/mp3;base64,' + b64encode(fp.read()).decode('UTF-8'), 'audio/mp3', autoplay=False)
+        mc.block_until_active()
+        time.sleep(1)
+        self.dev.set_volume(volume)
+        time.sleep(0.2)
+        mc.play()
+        while not mc.status.player_is_idle:
+            time.sleep(0.5)
+        mc.stop()
